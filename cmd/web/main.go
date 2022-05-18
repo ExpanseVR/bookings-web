e13adfbe073dc51351968db708a16b66cd2198cf
Michael Hayter <michaelhayter@expansevrdev.com>
2022-05-18 23:15:14 +0800

    Added forms and reservation summary page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/ExpanseVR/bookings/internal/config"
 	"github.com/ExpanseVR/bookings/internal/handlers"
+	"github.com/ExpanseVR/bookings/internal/models"
 	"github.com/ExpanseVR/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -19,6 +21,7 @@ const portNumber = ":8080"
 var session *scs.SessionManager
 
 func main() {
+	gob.Register(models.Reservation{})
 	//change this to true when in production
 	app.InProduction = false
 
